10f1ca9236b64eb67d96b05a8c2b858d624b5325
Pavithra Ranasinghe <paviranasinghe20@gmail.com>
2023-11-10 12:12:17 +0530

    Initialize the project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.yellow.shade200,
+        body: Center(
+          child: Container(
+
+          )
+          ),
+      ),
+    );
+  }
+}
